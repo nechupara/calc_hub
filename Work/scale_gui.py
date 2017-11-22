3c86d94f57cb9d19cf171e825c992550046ea359
nechupara <nechupara@gmail.com>
2017-11-22 19:18:14 +0200

    befor chooser

--- Work/scale_gui.py
+++ Work/scale_gui.py
@@ -3,22 +3,33 @@ from tkinter import *
 class Label_1(Label):
     def __init__(self, master):
         Label.__init__(self, master)
-        # self.config(text='Масштаб')
+        self.config(text='Масштаб')
 
 class Entry_1(Entry):
     def __init__(self, master):
         Entry.__init__(self,  master)
         self.insert(0,'enter')
+        
+class Frame_1(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, background='green')
+        
 
 root = Tk()
 root.title('Масштаб')
 root.geometry('500x400+200+200')
+root.tk_setPalette(background='purple')
 
-label_scale = Label_1(root)
-label_scale.config(justify=RIGHT, text='масштаб', width=5,  bg = 'gray', anchor=W, padx=0, pady=0)
-label_scale.place(x=10, y=10, anchor='nw')
+frame_input = Frame_1(root)
+frame_input['background']='red'
+frame_input.place(width=450, anchor='nw', height=30, x=20, y=20)
 
-# entry_scale1 = Entry_1(root)
-# entry_scale1.place(x=50, y = 10)
+label_scale = Label_1(frame_input)
+label_scale.config(justify=RIGHT, text='масштабsssss', height=2,  bg = 'gray', padx=0, pady=0)
+label_scale.grid(row=10, column=10, padx=5)
+
+entry_scale1 = Entry_1(frame_input)
+entry_scale1.configure(bg='green', width=10, font='Arial 10')
+entry_scale1.grid(row=10, column = 13, sticky='n')
 
 root.mainloop()
