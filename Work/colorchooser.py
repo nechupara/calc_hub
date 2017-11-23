cbdf83f4b8eb544fdeb28ba01c3b95cbd18caad9
Serge Dudko <31180567+nechupara@users.noreply.github.com>
2017-11-23 17:03:23 +0200

    Add files via upload
    23.11.2017

--- Work/colorchooser.py
+++ Work/colorchooser.py
@@ -1,12 +1,36 @@
 from tkinter import *
 from tkinter import colorchooser
+import time
 
-# print(colorchooser.winfo_id())
-# print(k)
-# print(k[1])
 
 def func_exit():
     pass
+
+def not_change_hex(event):
+    # entry_hex.icursor(0)
+    # entry_hex.delete(0, END)
+    hex_var.set(value=k[1])
+    entry_hex.icursor(0)
+    entry_hex.select_from(0)
+    entry_hex.select_to(END)
+
+
+def not_change_dec(event):
+    # entry_dec.icursor(0)
+    # entry_dec.delete(0, END)
+    dec_var.set(value='(' + str(int(k[0][0])) + ',' + str(int(k[0][1])) +',' + str(int(k[0][2])) +')')
+    entry_dec.icursor(0)
+    entry_dec.select_from(0)
+    entry_dec.select_to(END)
+
+
+def change(event):
+    # time.sleep(0.01)
+    hex_var.set(value='0000000000')
+    entry_hex.icursor(0)
+    entry_hex.select_from(0)
+    entry_hex.select_to(END)
+
 
 
 root=Tk()
@@ -31,54 +55,43 @@ text_hex.grid(row=10, column=15)
 frame_ent = Frame(root, bg='purple')
 frame_ent.place(x=20, y=70, width=260)
 
+dec_var = StringVar()
+dec_var.set(value='11111')
 hex_var = StringVar()
+hex_var.set(value='00000')
 
-entry_dec = Entry(frame_ent)
-entry_dec.insert(0,'nonenoe')
+entry_dec = Entry(frame_ent, textvariable=dec_var)
 entry_dec.grid(row = 10, column= 10, padx=3)
 
 entry_hex = Entry(frame_ent, textvariable=hex_var)
-entry_hex.insert(0,'nonenone')
 entry_hex.grid(row=10, column=15)
 
 root1=Tk()
 root1.geometry('+500+300')
-k = colorchooser.askcolor(parent=root1,color='#000000')
+root1.overrideredirect(1)
+k = colorchooser.askcolor(parent=root1)
 root1.destroy()
 
-# root.focus_get()
 
 text_dec['text']='(' + str(int(k[0][0])) + ',' + str(int(k[0][1])) +',' + str(int(k[0][2])) +')'
 text_hex['text']=k[1]
 
-entry_dec.delete(0,END)
-entry_hex.delete(0,END)
+dec_var.set(value='(' + str(int(k[0][0])) + ',' + str(int(k[0][1])) +',' + str(int(k[0][2])) +')')
+hex_var.set(value=k[1])
 
-entry_dec.insert(0,'(' + str(int(k[0][0])) + ',' + str(int(k[0][1])) +',' + str(int(k[0][2])) +')')
-entry_hex.insert(0,k[1])
 
-# root.grab_set()
+entry_hex.focus_set()
+entry_hex.select_to(END)
 
-# text_dec = Label(frame_info)
-# text_dec.config(text='(' + str(int(k[0][0])) + ',' + str(int(k[0][1])) +',' + str(int(k[0][2])) +')')
-# text_dec.grid(row = 10, column= 10, padx=3)
+entry_hex.bind("<KeyPress>", not_change_hex)
+entry_hex.bind("<KeyRelease>", not_change_hex)
+entry_hex.bind("<ButtonRelease-3>", change)
+# entry_hex.bind("<Button-1-KeyPress>", not_change)
 
-# text_hex = Label(frame_info)
-# text_hex.config(text=k[1])
-# text_hex.grid(row=10, column=15)
+entry_dec.bind("<KeyPress>", not_change_dec)
+entry_dec.bind("<KeyRelease>", not_change_dec)
+entry_dec.bind("<ButtonRelease>", not_change_dec)
 
-# frame_ent = Frame(root, bg='purple')
-# frame_ent.place(x=20, y=70, width=260)
-#
-# hex_var = StringVar()
-#
-#
-# entry_dec = Entry(frame_ent)
-# entry_dec.grid(row = 10, column= 10, padx=3)
-#
-# entry_hex = Entry(root, textvariable=hex_var)
-# entry_hex.insert(0,k[1])
-# entry_hex.grid(row=10, column=15)
 
 root.mainloop()
 print(k[0])
