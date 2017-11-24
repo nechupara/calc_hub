268fc496f6fc0c422502d0d65dc33c4ff386c4f6
Serge Dudko <31180567+nechupara@users.noreply.github.com>
2017-11-24 15:40:40 +0200

    Add files via upload
    24.11.2017

--- Work/colorchooser.py
+++ Work/colorchooser.py
@@ -35,41 +35,49 @@ def change(event):
 
 root=Tk()
 root.title('Номер цвета')
-root.geometry('300x200+600+300')
+root.geometry('+600+300')
 root.config(bg='#369d0b')
-root.resizable(width=False, height=False)
+root.resizable(width=0, height=0)
 # root.protocol('WM_DELETE_WINDOW', func=func_exit)
 
-frame_info = Frame(root, bg='gray')
-frame_info.place(x=20, y=20, width=260)
+frame_info = Frame(root, bg='#369d0b')
+frame_info.grid(row=10, column=10, padx=20, pady=20)
+# frame_info.grid(row=1, column=1, padx=20, pady=20, sticky= 'n')
+
 
 text_dec = Label(frame_info)
-text_dec.config(text='none')
-text_dec.grid(row = 10, column= 10, padx=3)
+text_dec.config(text='none', width=15, borderwidth=0, bg='#369d0b')
+text_dec.grid(row=10, column=10, ipadx=0)
+
+text_razdelitel_10 = Label(frame_info, width=0, bg='#369d0b', padx=0)
+text_razdelitel_10.grid(row=10, column=15)
 
 text_hex = Label(frame_info)
-text_hex.config(text='none')
-text_hex.grid(row=10, column=15)
+text_hex.config(text='none', width=15, bg='#369d0b')
+text_hex.grid(row=10, column=20)
 
 
-frame_ent = Frame(root, bg='purple')
-frame_ent.place(x=20, y=70, width=260)
+# frame_ent = Frame(root, bg='purple')
+# frame_ent.place(x=20, y=70, width=260)
 
 dec_var = StringVar()
 dec_var.set(value='11111')
 hex_var = StringVar()
 hex_var.set(value='00000')
 
-entry_dec = Entry(frame_ent, textvariable=dec_var)
-entry_dec.grid(row = 10, column= 10, padx=3)
+entry_dec = Entry(frame_info, textvariable=dec_var, justify='center', width=25)
+entry_dec.grid(row = 20, column= 10)
 
-entry_hex = Entry(frame_ent, textvariable=hex_var)
-entry_hex.grid(row=10, column=15)
+entry_razdelitel_20 = Label(frame_info, width=0, bg='#369d0b', padx=0)
+entry_razdelitel_20.grid(row=20, column=15)
+
+entry_hex = Entry(frame_info, textvariable=hex_var, justify='center', width=25)
+entry_hex.grid(row=20, column=20)
 
 root1=Tk()
 root1.geometry('+500+300')
 root1.overrideredirect(1)
-k = colorchooser.askcolor(parent=root1)
+k = colorchooser.askcolor(parent=root1, color='#369d0b')
 root1.destroy()
 
 
@@ -85,7 +93,7 @@ entry_hex.select_to(END)
 
 entry_hex.bind("<KeyPress>", not_change_hex)
 entry_hex.bind("<KeyRelease>", not_change_hex)
-entry_hex.bind("<ButtonRelease-3>", change)
+entry_hex.bind("<ButtonRelease>", not_change_hex)
 # entry_hex.bind("<Button-1-KeyPress>", not_change)
 
 entry_dec.bind("<KeyPress>", not_change_dec)
