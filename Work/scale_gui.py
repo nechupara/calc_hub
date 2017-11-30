790880207bc4378640f3e034e015f07c1956db44
nechupara <nechupara@gmail.com>
2017-11-30 20:25:44 +0200

    02

--- Work/scale_gui.py
+++ Work/scale_gui.py
@@ -3,40 +3,100 @@ from tkinter import *
 class Label_1(Label):
     def __init__(self, master):
         Label.__init__(self, master)
-        self.config(text='Масштаб')
+        self.config(bg='#ffeaf4')
 
 class Entry_1(Entry):
     def __init__(self, master):
         Entry.__init__(self,  master)
-        self.insert(0,'enter')
-        
+        self.config(bg='#b9b9dd')
+
 class Frame_1(Frame):
     def __init__(self, master):
-        Frame.__init__(self, master, background='green')
+        Frame.__init__(self, master)
         
 
 class Frame_1(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
 
+class Text_1(Text):
+    def __init__(self, master):
+        Text.__init__(self, master)
+        self.config(bg='green')
+
+
+class Button_1(Button):
+    def __init__(self, master):
+        Button.__init__(self, master)
+        self.configure(bg='purple')
 
 root = Tk()
 root.title('Масштаб')
+root.geometry('+600+300')
+root.tk_setPalette(background='#55ffaa')
 
-root.geometry('500x400+200+200')
-root.tk_setPalette(background='purple')
+frame_root = Frame_1(root)
+frame_root.grid(row=0, column=0, padx=20, pady=20)
 
-frame_input = Frame_1(root)
-frame_input['background']='red'
-frame_input.place(width=450, anchor='nw', height=30, x=20, y=20)
+frame_input = Frame_1(frame_root)
+frame_input.grid(row=10, column=10, sticky=W)
 
-label_scale = Label_1(frame_input)
-label_scale.config(justify=RIGHT, text='масштабsssss', height=2,  bg = 'gray', padx=0, pady=0)
-label_scale.grid(row=10, column=10, padx=5)
+label_in_scale=Label_1(frame_input)
+label_in_scale.config(text="Масштаб")
+label_in_scale.grid(row=10, column=10)
+
+label_razd0 = Label_1(frame_input)
+label_razd0.grid(row=10, column=15, padx=5)
 
 entry_scale1 = Entry_1(frame_input)
-entry_scale1.configure(bg='green', width=10, font='Arial 10')
-entry_scale1.grid(row=10, column = 13, sticky='n')
+entry_scale1.config(width=6, justify='c')
+entry_scale1.insert(0,1)
+entry_scale1.grid(row=10, column=20)
 
+label_razd1 = Label_1(frame_input)
+label_razd1.configure(text=':')
+label_razd1.grid(row=10, column=25)
+
+entry_scale2 = Entry_1(frame_input)
+entry_scale2.config(width=8, justify=CENTER)
+entry_scale2.grid(row=10, column=30)
+
+
+#Вывод
+frame_output = Frame_1(frame_root)
+frame_output.configure(pady=20)
+frame_output.grid(row=20, column=10, sticky=W)
+
+text_out_00 = Label_1(frame_output)
+text_out_00.configure(height=1, width=7, text='в 1 мм')
+text_out_00.grid(row=10, column=10, sticky=W)
+
+text_out_01 = Label_1(frame_output)
+text_out_01.grid(row=10, column=20)
+
+text_out_02 = Label_1(frame_output)
+text_out_02.configure(text='м')
+text_out_02.grid(row=10, column=30)
+
+
+text_out_10 = Label_1(frame_output)
+text_out_10.configure(height=1, width=7, text='в 1 м')
+text_out_10.grid(row=20, column=10, sticky=W)
+
+text_out_11 = Label_1(frame_output)
+text_out_11.grid(row=20, column=20)
+
+text_out_12 = Label_1(frame_output)
+text_out_12.configure(text='мм')
+text_out_12.grid(row=20, column=30)
+
+
+#кнопки
+frame_button = Frame_1(frame_root)
+frame_button.grid(row=30, column=10)
+
+button_do = Button_1(frame_button)
+button_do.configure(text='Рассчитать')
+button_do.grid(row=10, column=10)
 
 root.mainloop()
--- Work/temp.py
+++ Work/temp.py
@@ -1,1 +1,46 @@
-print('%x' % 15)
+from tkinter import *
+
+root = Tk()
+root.title('text')
+root.geometry('600x400')
+
+frame1 = Frame(root, bg='green')
+frame1.pack(side=LEFT)
+
+label1 = Label(frame1, text='label-1')
+label1.grid(row=0, column=0)
+
+frame2 = Frame(root)
+frame2.pack(side=LEFT)
+
+label2 = Label(frame2, text='label-2')
+label2.grid(row=0, column=0)
+
+
+frame3 = Frame(root)
+frame3.pack()
+
+label3 = Label(frame3, text='label-3')
+label3.grid(row=0, column=0)
+
+
+root.mainloop()
+
+
+
+# from tkinter import *
+#
+# def onclick():
+#    pass
+#
+# root = Tk()
+# text = Text(root)
+# text.insert(INSERT, "Hello.....")
+# text.insert(END, "Bye Bye.....")
+# text.pack()
+#
+# text.tag_add("here", "1.0", "1.4")
+# text.tag_add("start", "1.8", "1.13")
+# text.tag_config("here", background="yellow", foreground="blue")
+# text.tag_config("start", background="black", foreground="green")
+# root.mainloop()
